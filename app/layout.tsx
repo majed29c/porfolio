f1b616f1e196ad261372630f23c78ef53cd82e6d
majed29c <majedshmaitlu@gmail.com>
2025-05-02 15:52:08 +0300

    edits on layout.ts

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { SelectedProvider } from "@/context/SelectedContext";
 
 import Navbar from "@/components/Navbar/Navbar";
 import { BackgroundBeams } from "@/components/ui/background-beams";
+import { useEffect, useState } from "react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,6 +27,24 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768); // Set to true for screen widths <= 768px
+    };
+
+    // Initial check
+    handleResize();
+
+    // Add event listener for window resize
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <html lang="en">
       <head>
@@ -34,7 +53,7 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable} bg-black`}>
         <SelectedProvider>
           <Navbar />
-          <BackgroundBeams className="fixed z-0" />
+          {!isMobile && <BackgroundBeams className="fixed z-0" />}
           {children}
         </SelectedProvider>
       </body>
